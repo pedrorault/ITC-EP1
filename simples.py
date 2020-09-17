36418f0a369be9b6871b0d81ce5980c0f911afde
Pedro R Costa <pedrorault@gmail.com>
2020-09-17 15:37:04 -0300

    #FIXME pra questao mais urgente, TODO: Saida em arquivo

--- simples.py
+++ simples.py
@@ -1,12 +1,41 @@
 import sys
 
+def existeTransicao(stAtual,digito,stDestino):
+    if stDestino:
+        if int(stDestino[2]) == 0 or stDestino.startswith(f'{stAtual} {digito}'):
+            return True
+    return False
+
+def compararVTesteEstado(stAtual,teste,listaTransicao):
+    finaliza = [stAtual]
+    #FIXME: Nao consumir digito quando a cadeia lida eh vazia
+
+    for digito in teste:
+        stTempList = finaliza[:]
+        finaliza = []
+        stDestinoList = []
+        for stTemp in stTempList: 
+            for transicao in listaTransicao:
+                if int(transicao[0]) == stTemp and existeTransicao(stTemp,digito,transicao):
+                    stDestino = transicao[4]                    
+                    stDestinoList.append(stDestino)
+        stDestinoList = filter(lambda x: x not in finaliza, stDestinoList)
+        finaliza += map(lambda x : int(x), stDestinoList)
+    return finaliza
+
+def checkFinalizadoAceito(finalizadoEm,aceitacaoList):
+    for stFinalizado in finalizadoEm:
+        if int(stFinalizado) in aceitacaoList:
+            return 1
+    return 0
+
 def main():
     nomeEntrada = sys.argv[2]
     nomeSaida = sys.argv[4]
 
     with open(nomeEntrada) as f:
         numAutomatos = int(f.readline())
-        for i in range(0,numAutomatos):
+        for i in range(0,numAutomatos):      
             definicao = map(lambda x : int(x), f.readline().replace("\t"," ").split(" "))
             aceitacaoList = list(map(lambda x : int(x), f.readline().split(" ")))
 
@@ -22,24 +51,15 @@ def main():
                     teste = list(map(lambda x : int(x), line.replace("\t"," ").split(" ")))
                 testeList.append(teste)
 
-            #do tests and write to file
             stAtual = stInicial
-            resultado =  []
-            # 121
-            for digito in testeList[0]:
-                substr = f'{stAtual} {digito}'
-                for transi in transiList:
-                    if transi.startswith(substr):
-                        stAtual = transi[-1]
-                        break
-         
-
-                        
-            #if stAtual in aceitacaoList:   return 1 else 0
+            total = []
+            
+            for teste in testeList:
+                stFinalizados = compararVTesteEstado(stAtual,teste,transiList)
+                foiAceito = checkFinalizadoAceito(stFinalizados,aceitacaoList)
+                total.append(foiAceito)
+            print("resultado: "," ".join(map(lambda x: str(x), total)))
+            #TODO: Saida como arquivo           
 
 if __name__ == "__main__":
-    # main()
-    x = "0 1"
-    y = ["1 1 3", "0 0 2", "0 1 3"]
-    if y[2].startswith(x):
-        print("yes")
+    main()
